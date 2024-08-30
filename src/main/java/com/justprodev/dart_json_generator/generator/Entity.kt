8b162f3fb470f60d347dac89a5868b67155cc2f4
Емельянов Александр <a.yemelyanov@gmail.com>
2024-08-31 01:15:17 +0500

    fix class name

--- src/main/java/com/justprodev/dart_json_generator/generator/Entity.kt
+++ src/main/java/com/justprodev/dart_json_generator/generator/Entity.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
+import com.justprodev.dart_json_generator.utils.toCamelCase
 import com.justprodev.dart_json_generator.utils.toUpperCaseFirstOne
 
 /**
@@ -40,7 +41,7 @@ private class ObjectEntity(
     override val name: String,
     override val children: List<Entity>?
 ) : Entity(name, children) {
-    override val type = name.toUpperCaseFirstOne()
+    override val type = name.toUpperCaseFirstOne().toCamelCase()
 }
 
 /**
